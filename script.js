879040356b85486fa96007bd663a80f1a912db8a
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-09-24 19:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function stringChop(str, size) {
   // your code here
+	let ans=[];
+	let i=0;
+	let j=0;
+	while(i<str.length){
+		let result=""
+		for(let k=0;k<size;k++,i++){
+			if(i<str.length){
+				result+=str[i]
+			}else{
+				break
+			}
+		}
+		ans[j++]=result;
+	}
+	return ans;
 }
 
 // Do not change the code below
